383e87d1a341a78e2527c2af6d5d111aa47e728a
Logan Saunders <lsaunders275@gmail.com>
2020-02-05 20:54:46 -0700

    started working on rust

---
+++ rust/target/debug/deps/hello_cargo-808599da8e18c98f.d
@@ -0,0 +1,5 @@
+C:\Users\lsaun\Downloads\hello_cargo\target\debug\deps\hello_cargo-808599da8e18c98f.rmeta: src\main.rs
+
+C:\Users\lsaun\Downloads\hello_cargo\target\debug\deps\hello_cargo-808599da8e18c98f.d: src\main.rs
+
+src\main.rs:
---
+++ rust/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\lsaun\Documents\word-ladder\rust\target\debug\hello_cargo.exe: C:\Users\lsaun\Documents\word-ladder\rust\src\main.rs
