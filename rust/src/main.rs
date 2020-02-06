c940ee856c345161aee174f769469f14d479c22b
Logan Saunders <lsaunders275@gmail.com>
2020-02-05 22:07:19 -0700

    Created a function to get user input and store it in a string

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+use std::io::Write; // bring flush() inot scope
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the Word Ladder!\n");
+    let first_word = get_word_from_user("Please enter the first word: ");
+    let last_word = get_word_from_user("Please enter the last word: ");
 }
+
+fn get_word_from_user(message: &str) -> std::string::String {
+    print!("{}", message);
+    io::stdout().flush().unwrap(); // flushes stdout so that the message appears
+
+    let mut word = String::new();
+    io::stdin()
+        .read_line(&mut word)
+        .expect("Failed to read line");
+
+    return word;
+}
+
+// get the start word;
+// get end word;
+// make sure both words are the same length
+// get length of word
+// read in lexicon
+// keep only words that are the same length; would this be going through the lexicon twice?
+// start ladder; create new queue (stack?)
+// write function to determine if there is a one word difference between two words
+// if there is only one word difference, add that word to the stack
+// copy stack
+// put that stack into a vector
+// continue for next word until we reach the end of the lexicon
+// start over with each ladder in vector until the end word is found in one stack
+// we found the shortest ladder
+// display ladder to user
