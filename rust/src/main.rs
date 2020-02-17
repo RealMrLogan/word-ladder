f2ba4d3e4af3e6fc58eb0a28a4241ef4322df54d
Logan Saunders <lsaunders275@gmail.com>
2020-02-16 20:40:49 -0700

    read in the lexicon and convereted it into a hashmap

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,14 +1,20 @@
 use std::io;
 use std::io::Write; // bring flush() into scope
+use std::collections::HashMap;
+use std::fs;
+use std::convert::TryInto;
 
 fn main() {
     println!("Welcome to the Word Ladder!\n");
     let first_word = get_word_from_user("Please enter the first word: ");
     let last_word = get_word_from_user("Please enter the last word: ");
-    // this measures bytes, not number of characters?
-    if first_word.chars().count() == last_word.chars().count() {
-        println!("The two words are the same!");
+    if first_word.chars().count() != last_word.chars().count() {
+        println!("The two words are NOT the same length!");
     }
+
+    let lexicon = fs::read_to_string("../lexicon.txt").expect("Unable to read file");
+    let lexicon_map = put_lexicon_into_hashmap(lexicon); // would this be inefficient? I could just iterate through the string once?
+    // TODO: pick up here.
 }
 
 fn get_word_from_user(message: &str) -> std::string::String {
@@ -20,15 +26,25 @@ fn get_word_from_user(message: &str) -> std::string::String {
         .read_line(&mut word)
         .expect("Failed to read line");
 
-    return word;
+    return word.trim().to_string();
 }
 
-// get the start word;
-// get end word;
-// make sure both words are the same length
-// get length of word
-// read in lexicon
-// keep only words that are the same length; would this be going through the lexicon twice?
+fn put_lexicon_into_hashmap(lexicon: String) -> HashMap<i32, Vec<String>> {
+    let mut lexicon_map: HashMap<i32, Vec<String>> = HashMap::new();
+    let split: Vec<&str> = lexicon.split("\n").collect();
+    for word in split {
+        let num_chars: i32 = word.chars().count().try_into().unwrap();
+        // if the key (entry) is there, push onto the vector, if it's not there, create an new vector and push
+        lexicon_map.entry(num_chars).or_insert(Vec::new()).push(word.to_string());
+    }
+    return lexicon_map;
+}
+
+// *get the start word;
+// *get end word;
+// *make sure both words are the same length
+// *get num chars of word
+// *read in lexicon into a map wit hthe num chars as a key and array of words as value
 // start ladder; create new queue (stack?)
 // write function to determine if there is a one word difference between two words
 // if there is only one word difference, add that word to the stack
