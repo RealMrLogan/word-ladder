09203122233164dd8f7981e79b0aae85c6df89c7
Logan Saunders <lsaunders275@gmail.com>
2020-02-16 21:31:11 -0700

    stubbed out function to get word_ladder

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 use std::io::Write; // bring flush() into scope
 use std::collections::HashMap;
+use std::collections::VecDeque;
 use std::fs;
 use std::convert::TryInto;
 
@@ -15,6 +16,7 @@ fn main() {
     let lexicon = fs::read_to_string("../lexicon.txt").expect("Unable to read file");
     let lexicon_map = put_lexicon_into_hashmap(lexicon); // would this be inefficient? I could just iterate through the string once?
     // TODO: pick up here.
+    let word_ladder = find_word_ladder(lexicon_map, first_word, last_word);
 }
 
 fn get_word_from_user(message: &str) -> std::string::String {
@@ -40,14 +42,33 @@ fn put_lexicon_into_hashmap(lexicon: String) -> HashMap<i32, Vec<String>> {
     return lexicon_map;
 }
 
+fn find_word_ladder(mut lexicon_map: HashMap<i32, Vec<String>>, first_word: String, last_word: String) -> Vec<String> {
+    let word_ladders: VecDeque<Vec<String>> = VecDeque::new();
+    let key: i32 = first_word.chars().count().try_into().unwrap();
+    // returns an Option
+    let lexicon_value = lexicon_map.get(&key);
+    println!("Value: {:?}", lexicon_value);
+
+    let word_ladder = Vec::new();
+
+    return word_ladder;
+}
+
+fn is_one_letter_different(word: String, comparer: String) -> bool {
+    let mut is_one_letter: bool = false;
+    let mut is_more_than_one_letter: bool = false;
+
+    return is_one_letter;
+}
+
 // *get the start word;
 // *get end word;
 // *make sure both words are the same length
 // *get num chars of word
 // *read in lexicon into a map wit hthe num chars as a key and array of words as value
 // start ladder; create new queue (stack?)
-// write function to determine if there is a one word difference between two words
-// if there is only one word difference, add that word to the stack
+// write function to determine if there is a one letter difference between two words
+// if there is only one letter difference, add that word to the stack
 // copy stack
 // put that stack into a vector
 // continue for next word until we reach the end of the lexicon
